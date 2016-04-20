bc705935115867431a769f570cd1d8759675d072
wzhuangfu@hotmail.com <wzhuangfu@hotmail.com>
2016-04-20 15:54:05 +0800

    create

---
+++ CapQueen.Cache/CapQueen.Cache/ICacheHelper.cs
@@ -0,0 +1,23 @@
+﻿/*
+* Copyright (c) 2015,上海汇航捷讯网络科技有限公司
+* All rights reserved.
+* 
+* 摘    要：
+*
+* 当前版本：1.0
+* 作    者：Gondar
+* 创建日期：2016/4/20 15:07:45
+*
+* 修改记录：
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapQueen.Cache
+{
+    class ICacheHelper
+    {
+    }
+}
