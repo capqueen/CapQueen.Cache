9e9e21ea885c64fa8ae28a374c3ae5184295df72
wzhuangfu@hotmail.com <wzhuangfu@hotmail.com>
2016-04-20 19:52:02 +0800

    create

---
+++ CapQueen.Cache/CapQueen.Cache/Model/User.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Copyright (c) 2015,CapQueen
+* All rights reserved.
+* 
+* 摘    要：
+*
+* 当前版本：1.0
+* 作    者：CapQueen
+* 创建日期：2016/4/20 19:20:31
+*
+* 修改记录：
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapQueen.Cache.Model
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User
+    {
+        public int UserId { get; set;}
+
+        public string UserName { get; set; }
+
+        public string Cellphone { get; set; }
+    }
+}
